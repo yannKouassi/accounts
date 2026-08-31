36a3fda3f4167136a8017c16577d76263309d113
Yannick Kouassi <ynnkouassi@gmail.com>
2026-08-31 11:47:58 +0000

    creation de l'interface AccountRepository.java

---
+++ src/main/java/tech/chilo/accounts/AccountRepository.java
@@ -0,0 +1,8 @@
+package tech.chilo.accounts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account,Integer> {
+}
