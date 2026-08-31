7ded58e41dbfc57181afad9e03d1b617130ea846
Yannick Kouassi <ynnkouassi@gmail.com>
2026-08-31 11:21:22 +0000

    Initial commit

---
+++ src/main/java/tech/chilo/accounts/AccountsApplication.java
@@ -0,0 +1,13 @@
+package tech.chilo.accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsApplication {
+
+    public static void main ( String[] args ) {
+        SpringApplication.run ( AccountsApplication.class , args );
+    }
+
+}
