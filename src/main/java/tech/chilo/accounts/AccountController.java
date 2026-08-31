53908bbb4b8f73a6cdfc19419514f666f1643ba4
Yannick Kouassi <ynnkouassi@gmail.com>
2026-08-31 12:22:40 +0000

    amélioration du controller. ajout de consumes et produces

--- src/main/java/tech/chilo/accounts/AccountController.java
+++ src/main/java/tech/chilo/accounts/AccountController.java
@@ -3,6 +3,7 @@ package tech.chilo.accounts;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,14 +16,14 @@ import java.util.List;
 public class AccountController {
     private final AccountService accountService;
 
-    @PostMapping
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public void create(@RequestBody Account account){
         this.accountService.create(account);
     }
 
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Account>> getAllAccounts(){
         return ResponseEntity.ok (this.accountService.search ()  );
     }
