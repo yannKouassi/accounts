9a66632cfa14a85770a927e0b057fbd566a23bb9
Yannick Kouassi <ynnkouassi@gmail.com>
2026-08-31 11:52:11 +0000

    creation et implémentation des methodes du service AccountService.java

--- src/main/java/tech/chilo/accounts/AccountService.java
+++ src/main/java/tech/chilo/accounts/AccountService.java
@@ -1,4 +1,23 @@
 package tech.chilo.accounts;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class AccountService {
+    private final AccountRepository accountRepository;
+
+
+    public void createAccount(Account account){
+        this.accountRepository.save(account);
+    }
+
+    public List<Account> search() {
+        return this.accountRepository.findAll();
+    }
+
 }
